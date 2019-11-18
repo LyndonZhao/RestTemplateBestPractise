be5f0cd6df55889ed24b6dc737b2be817ae7b01b
Lyndon201 <hui85201@163.com>
2019-11-18 22:48:35 +0800

    [Title] initial comment
    [Modification]
    1) initial comment
    
    Signed-off-by: Lyndon201 <hui85201@163.com>

---
+++ src/test/java/pn/lyndon/resttemplate/ResttemplateApplicationTests.java
@@ -0,0 +1,12 @@
+package pn.lyndon.resttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResttemplateApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
